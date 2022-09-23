83f02303e82a5bdf7132d643897c8363a1ad62c2
Soumen Maji <109405097+Simplysoumen@users.noreply.github.com>
2022-09-23 14:58:51 +0530

    Create InputOutput.py

---
+++ InputOutput.py
@@ -0,0 +1,13 @@
+#Print int$float$character
+N,M, ch= input().split()
+N = int(N)
+M = float(M)
+print("%d$%.2f$%c"%(N,M,ch))
+
+#Print Character
+ch = input()
+print("%c"%(ch))
+
+#Print
+N, M = map(float, input().split())
+print("%.2f %.2f"%(N,M))

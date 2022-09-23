d186d48165b4e72b1cddacaba0ed5e981ea8605d
Soumen Maji <109405097+Simplysoumen@users.noreply.github.com>
2022-09-23 15:02:31 +0530

    Create Operators.py

---
+++ Operators.py
@@ -0,0 +1,35 @@
+a = 5
+b = 4
+c = 6.984
+d = 6
+st= "Name"
+#Relational Operators
+print(a>b)
+print(a<b)
+print(a!=c)
+print(a==c)
+print(d==c)
+print(d<=c)
+print(d>=6)
+
+print(a>b and d>=6)
+print(a<b or d>=6)
+
+#Arithmatic Operators
+print(a+b)
+print(a-b)
+print(c-a)
+print(a*c)
+print(a/b)
+print(a//b)
+print(a/c)
+print(a//c)
+
+
+print(a**b)
+
+#Bitwise Operators
+print("Bitwise Operators")
+print(2&3)
+print(2|3)
+print(2^3)

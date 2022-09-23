ee2029d64469731e506d66140c84f926bf0255ee
Soumen Maji <109405097+Simplysoumen@users.noreply.github.com>
2022-09-23 15:05:52 +0530

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,17 @@
+VarInt=5
+a = 10
+b = 20
+VarStr= "PrepBytes"
+print(VarInt, a,b,VarStr)
+print(a+b+VarInt)
+
+print(type(VarStr))
+print(type(a))
+
+a= "Hello"
+print(a)
+print(type(a))
+
+a = 1.5
+print(a)
+print(type(a))

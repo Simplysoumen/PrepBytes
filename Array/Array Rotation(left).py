c547030211e33386ffcbd35fccdec1dfcf16f31d
Soumen Maji <109405097+Simplysoumen@users.noreply.github.com>
2022-09-27 16:04:27 +0530

    Create Array Rotation(left).py

---
+++ Array/Array Rotation(left).py
@@ -0,0 +1,23 @@
+def leftRotate(arr,n):
+    temp=arr[n-1]
+    for i in range():
+            arr[i-1]=arr[i]
+    arr[0]=temp
+
+def printArr(arr, n):
+    for i in range(n):
+        print(arr[i], end=' ')
+    print()
+
+def main():
+    t=int(input())
+    while(t>0):
+        n,k=map(int,input().split())
+        arr=list(map(int,input().split()))
+        for i in range(k):
+                leftRotate(arr,n)
+                printArr(arr,n)
+                t=t-1
+
+if __name__=="__main__":
+    main()

b3d074f3cffd042e6c9b13aeaee96d030ae49a98
Soumen Maji <109405097+Simplysoumen@users.noreply.github.com>
2022-09-27 16:10:28 +0530

    Create InterestingArrayOptimized.py

---
+++ Array/InterestingArrayOptimized.py
@@ -0,0 +1,27 @@
+def main():
+    t = int(input())
+    while(t!=0):
+        n = int(input())
+        arr = list(map(int, input().split()))
+        k = int(input())
+        i=0
+        j=n-1
+        flag=0
+        while(i<j):
+            sum=arr[i]+arr[j]
+        if(sum==k):
+            flag=1
+            print(i,j)
+            break
+        elif(sum>k):
+            j=j-1
+        else:
+            i=i+1
+
+        if(flag==0):
+            print("No Answer")
+
+        t=t-1
+
+if __name__=='__main__':
+    main()

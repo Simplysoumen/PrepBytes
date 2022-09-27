a6b0b0253b6e1ef4ee9ce674d7515b4b2aa7383d
Soumen Maji <109405097+Simplysoumen@users.noreply.github.com>
2022-09-27 16:13:06 +0530

    Create Right Reverse Algo.py

---
+++ Array/Right Reverse Algo.py
@@ -0,0 +1,26 @@
+def reverseRightAlgo(arr,i,j):
+    while(i<j):
+        temp=arr[i]
+        arr[i]=arr[j]
+        arr[j]=temp
+        i=i+1
+        j=j-1
+
+def printArr(arr,n):
+    for i in range(n):
+        print(arr[i],end=' ')
+    print()
+
+def main():
+    t = int(input())
+    while(t>0):
+        n,k=map(int, input().split())
+        arr=list(map(int,input().split()))
+        reverseRightAlgo(arr,n-k,n-1)
+        reverseRightAlgo(arr,0,n-k-1)
+        reverseRightAlgo(arr,0,n-1)
+        printArr(arr,n)
+        t=t-1
+
+if __name__=='__main__':
+    main()

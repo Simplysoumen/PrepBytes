f5987ab39ea4a239b96ed6aea4af84053622175b
Soumen Maji <109405097+Simplysoumen@users.noreply.github.com>
2022-09-28 18:44:47 +0530

    Create DuplicateCharacters.py

---
+++ Strings/DuplicateCharacters.py
@@ -0,0 +1,18 @@
+t= int(input())
+while(t>0):
+    str = input()
+    freq = [0]*26
+    for i in range(len(str)):
+        freq[ord(str[i])-97]+=1
+
+    flag=0
+    for i in range(26):
+        if(freq[i]>1):
+            print('{}={}'.format(chr(i+97),freq[i],end=' '))
+            flag=1
+
+    if(flag==0):
+        print("No element is repeating")
+    else:
+        print()
+    t=t-1

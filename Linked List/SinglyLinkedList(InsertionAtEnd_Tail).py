a6a47c4c31aa756b4e0b663b08b0590643179dde
Soumen Maji <109405097+Simplysoumen@users.noreply.github.com>
2022-10-03 23:45:18 +0530

    Create SinglyLinkedList(InsertionAtEnd_Tail).py

---
+++ Linked List/SinglyLinkedList(InsertionAtEnd_Tail).py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+
+    def insertAtEnd(self,data):
+        newNode = Node(data)
+        if(self.head is None):
+            self.head=newNode
+            self.tail=newNode
+
+        else:
+            self.tail.next=newNode
+            self.tail=newNode
+
+    def printSinglyLinkedList(self):
+        temp=self.head
+        while(temp is not None):
+            print(temp.data, end=' ')
+            print(temp, end=' ')
+            print(temp.next)
+            temp=temp.next
+
+def main():
+    singlyLinkedList = LinkedList()
+    arr = list(map(int, input().split()))
+    for i in range(len(arr)):
+        singlyLinkedList.insertAtEnd(arr[i])
+
+    singlyLinkedList.printSinglyLinkedList()
+
+if __name__ == '__main__':
+    main()

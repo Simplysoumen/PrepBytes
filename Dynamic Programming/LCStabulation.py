3e818a26dd699eebd04e2db873a5b6f636eba05c
Soumen Maji <109405097+Simplysoumen@users.noreply.github.com>
2023-04-08 20:55:27 +0530

    Create LCStabulation.py

---
+++ Dynamic Programming/LCStabulation.py
@@ -0,0 +1,23 @@
+def main():
+    t = int(input())
+    while(t!=0):
+        st1 = input()
+        st2 = input()
+        n = len(st1)
+        m = len(st2)
+        dp = [[-1 for _ in range(m+1)for _ in range(n+1)]]
+        for i in range(n+1):
+            dp[i][0] = 0
+        for j in range(m+1):
+            dp[0][j] = 0
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if(st1[i-1] == st2[j-1]):
+                    dp[i][j] = 1+dp[i-1][j]
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        print(dp[n][m])
+        t = t-1
+
+if __name__ == '__main__':
+    main()
